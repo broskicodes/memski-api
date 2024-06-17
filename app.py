9709587437fd7b6e665ae7a2dae5f3f09e256e32
nibbus <nibbus0x@protonmail.com>
2024-06-17 08:43:54 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from mem0 import Memory
+from flask import Flask, request
+
+load_dotenv()
+app = Flask(__name__)
+
+config = {
+  "vector_store": {
+    "provider": "qdrant",
+    "config": {
+      "host": "localhost",
+      "port": 6333
+    }
+  }
+}
+
+mem_store = Memory.from_config(config)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route("/save", methods=["POST"])
+def save_memory():
+    json_data = request.get_json()
+    user_id = json_data.get("user_id")
+    memory = json_data.get("memory")
+
+    print(user_id)
+    print(memory)
+
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
