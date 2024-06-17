9066c79fb10ef947516444d83d016e5f66b189a4
nibbus <nibbus0x@protonmail.com>
2024-06-17 10:13:35 -0400

    make qdrant cloud work

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from mem0 import Memory
 from flask import Flask, request
@@ -5,36 +6,37 @@ from flask import Flask, request
 load_dotenv()
 app = Flask(__name__)
 
-# config = {
-#   "vector_store": {
-#     "provider": "qdrant",
-#     "config": {
-#       "host": "localhost",
-#       "port": 6333
-#     }
-#   }
-# }
+config = {
+  "vector_store": {
+    "provider": "qdrant",
+    "config": {
+      "url": f"{os.getenv('QDRANT_API_ENDPOINT', 'localhost')}:6333",
+      "api_key": os.getenv("QDRANT_API_KEY")
+    }
+  }
+}
 
-# mem_store = Memory.from_config(config)
+mem_store = Memory.from_config(config)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-# @app.route("/save", methods=["POST"])
-# def save_memory():
-#     json_data = request.get_json()
-#     user_id = json_data.get("user_id")
-#     memory = json_data.get("memory")
+@app.route("/save", methods=["POST"])
+def save_memory():
+    json_data = request.get_json()
+    user_id = json_data.get("user_id")
+    memory = json_data.get("memory")
 
-#     print(user_id)
-#     print(memory)
+    print(user_id)
+    print(memory)
 
-#     return "ok"
+    return "ok"
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
+    # app.run(debug=True)
 
 
 
