0c5cd9329074d6665a18dbec6ba9aff93e16576b
nibbus <nibbus0x@protonmail.com>
2024-06-17 09:19:51 -0400

    comment complex

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ from flask import Flask, request
 load_dotenv()
 app = Flask(__name__)
 
-config = {
-  "vector_store": {
-    "provider": "qdrant",
-    "config": {
-      "host": "localhost",
-      "port": 6333
-    }
-  }
-}
+# config = {
+#   "vector_store": {
+#     "provider": "qdrant",
+#     "config": {
+#       "host": "localhost",
+#       "port": 6333
+#     }
+#   }
+# }
 
-mem_store = Memory.from_config(config)
+# mem_store = Memory.from_config(config)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-@app.route("/save", methods=["POST"])
-def save_memory():
-    json_data = request.get_json()
-    user_id = json_data.get("user_id")
-    memory = json_data.get("memory")
+# @app.route("/save", methods=["POST"])
+# def save_memory():
+#     json_data = request.get_json()
+#     user_id = json_data.get("user_id")
+#     memory = json_data.get("memory")
 
-    print(user_id)
-    print(memory)
+#     print(user_id)
+#     print(memory)
 
-    return "ok"
+#     return "ok"
 
 
 if __name__ == '__main__':
