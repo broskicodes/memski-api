a3e5405c144d21c1ee3578c9afcf2b28abac4407
nibbus <nibbus0x@protonmail.com>
2024-06-22 15:02:41 -0400

    git commit -m

--- app.py
+++ app.py
@@ -18,13 +18,10 @@ config = {
   }
 }
 
+wordware_api_key = os.getenv("WORDWARE_API_KEY")
 mem_store = Memory.from_config(config)
 
-@app.route("/", methods=["GET"])
-def hello():
-  return "hello"
-
-@app.route('/query', methods=["POST"])
+@app.route('memory/query', methods=["POST"])
 def query_memories():
     json_data = request.get_json()
     user_id = json_data.get("user_id")
@@ -35,7 +32,7 @@ def query_memories():
 
     return [{'text': memory['text'], 'score': memory['score']} for memory in memories]
 
-@app.route("/save", methods=["POST"])
+@app.route("memory/save", methods=["POST"])
 def save_memory():
     json_data = request.get_json()
     user_id = json_data.get("user_id")
@@ -45,6 +42,31 @@ def save_memory():
     mem_store.add(memory, user_id=user_id, metadata=metadata)
 
     return "memory added successfully"
+
+@app.route("/prompt")
+def rewrite_prompt():
+    prompt_id = "5b95a4a2-5092-4b71-9276-ece797bc195f"
+
+    json_data = request.get_json()
+    agent_sys_prompt = json_data.get("agent_sys_prompt")
+    convo = json_data.get("convo")
+    prompt = json_data.get("prompt")
+    user_id = json_data.get("user_id") 
+
+    r = requests.post(f"https://app.wordware.ai/api/prompt/{prompt_id}/run",
+        json={
+            "inputs": {
+                "agent_sys_prompt": agent_sys_prompt,
+                "convo": convo,
+                "prompt": prompt,
+                "user_id": user_id,
+            }
+        },
+        headers={"Authorization": f"Bearer {wordware_api_key}"},
+        stream=False
+    )
+
+    return r
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=os.getenv("PORT", 5000))
